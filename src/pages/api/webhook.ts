eaa7904d1e501df23cd17ec437e74ec95306b1eb
Hamza Arif <hamza.arif5587@gmail.com>
2023-04-22 09:51:37 +0500

    fixes

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,7 +1,8 @@
-import * as admin from "firebase-admin";
-import { ServiceAccount } from "firebase-admin";
 import { buffer } from "micro";
 import { NextApiRequest, NextApiResponse } from "next";
+import * as admin from "firebase-admin";
+import { ServiceAccount } from "firebase-admin";
+import Stripe from "stripe";
 
 // Secure a connection to Firebase from backend
 const serviceAccount = {
@@ -12,59 +13,82 @@ const serviceAccount = {
     clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
 } as ServiceAccount;
 
-const app = !admin.apps.length
-    ? admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-    })
-    : admin.app();
+// Initialize Firebase app
+const app = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
 
 // Establish connection to Stripe
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
-const fulfillOrder = async (session: any) => {
-    return app
-        .firestore()
-        .collection("users")
-        .doc(session.metadata.email)
-        .collection("orders")
-        .doc(session.id)
-        .set({
-            amount: session.amount_total / 100,
-            amount_shipping: session.total_details.amount_shipping / 100,
-            images: JSON.parse(session.metadata.images),
-            timestamp: admin.firestore.FieldValue.serverTimestamp(),
-        })
-        .then(() => {
-            console.log(`SUCCESS: Order ${session.id} has been added to the DB`);
-        });
+
+// Function to fulfill an order
+const fulfillOrder = async (paymentIntent: Stripe.PaymentIntent | any) => {
+    const { metadata, id, amount_total, total_details } = paymentIntent;
+    const { email, images } = metadata;
+    console.log("paymentIntent", paymentIntent)
+    console.log("metaData", metadata)
+    const orderData = {
+        amount: amount_total / 100,
+        images: JSON.parse(images),
+        amount_shipping: (total_details?.amount_shipping ?? 0) / 100,
+        timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    };
+
+    try {
+        await app
+            .firestore()
+            .collection("users")
+            .doc(email)
+            .collection("orders")
+            .doc(id)
+            .set(orderData);
+
+        console.log(`SUCCESS: Order ${id} has been added to the DB`);
+    } catch (err) {
+        console.error("Error adding order to Firestore", err);
+        throw new Error("Error adding order to Firestore");
+    }
 };
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    if (req.method === "POST") {
-        const requestBuffer = await buffer(req);
-        const payload = requestBuffer.toString();
-        const sig = req.headers["stripe-signature"];
+    if (req.method !== "POST") {
+        res.setHeader("Allow", "POST");
+        return res.status(405).end("Method Not Allowed");
+    }
 
-        let event;
+    const requestBuffer = await buffer(req);
+    const payload = requestBuffer.toString();
+    const sig : any = req.headers["stripe-signature"];
 
-        // Verify that the Event posted came from Stripe
-        try {
-            event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
-        } catch (err: any) {
-            console.log("ERROR", err.message);
-            return res.status(400).send(`Webhook error: ${err.message}`);
+    let event: Stripe.Event;
+
+    // Verify that the Event posted came from Stripe
+    try {
+        event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+    } catch (err: any) {
+        console.error("Error verifying Stripe signature", err);
+        return res.status(400).end(`Webhook Error: ${err.message}`);
+    }
+
+    // Handle the event
+    try {
+        switch (event.type) {
+            case "checkout.session.completed":
+                const paymentIntent = event.data.object as Stripe.PaymentIntent;
+                await fulfillOrder(paymentIntent);
+                break;
+
+            // Handle other event types
+            default:
+                console.log(`Unhandled event type: ${event.type}`);
         }
-        // Handle the checkout.session.completed event
-        if (event.type === "checkout.session.completed") {
-            const session = event.data.object;
 
-            // Fullfil order
-            return fulfillOrder(session)
-                .then(() => res.status(200))
-                .catch((err) => {
-                    res.status(400).send(`Webhook Error: ${err.message}`);
-                });
-        }
+        res.status(200).json({ received: true });
+    } catch (err) {
+        console.error("Error handling Stripe event", err);
+        res.status(500).end("Internal Server Error");
     }
 };
 
