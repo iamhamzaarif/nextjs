70a310cecf9e2717ee8067cb83b280532e14cb73
Hamza Arif <hamza.arif5587@gmail.com>
2023-04-22 10:54:44 +0500

    margin top handling

--- src/components/ProductFeed.tsx
+++ src/components/ProductFeed.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const ProductFeed = ({ products }: Props) => {
   return (
-    <div className="grid grid-flow-row-dense -mt-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  md:-mt-52 lg:mt-75 xl:mt-20 mx-auto">
+    <div className="grid grid-flow-row-dense mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  md:-mt-75 lg:mt-75 xl:mt-20 mx-auto">
       {products.slice(8, 12).map((product) => (
         <Product key={product.id.toString()} product={product} />
       ))}
