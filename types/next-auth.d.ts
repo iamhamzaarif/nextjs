a9a0d25cc97c43bb13bfa5bd2be2360001032568
Hamza Arif <hamza.arif5587@gmail.com>
2023-04-25 02:25:13 +0500

    jwt Authentication

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -2,6 +2,7 @@ import {DefaultSession} from "next-auth"
 
 declare module "next-auth" {
     interface Session {
+        loggedUser: string,
         user: {
             address: string
         } & DefaultSession["user"]
